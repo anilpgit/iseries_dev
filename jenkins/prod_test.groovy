f615f710355f2d17d1b47c74d19b462a6d63db7b
anilpgit <ananbasil@gmail.com>
2025-05-12 19:27:45 -0400

    jenkins file

--- jenkins/prod_test.groovy
+++ jenkins/prod_test.groovy
@@ -1,33 +1,34 @@
 node {
-    stage('Cool stuff') {
+    stage('Save Restore') {
         onIBMi('PUB400') {
-            //Create a library
-            def library = 'COOLSTUFF'
-
-            ibmiCommand "CRTLIB LIB($library)"
-
+            //Create a SAVF in APINTO12
+            def library = 'APINTO12'
+            def savFile = 'RELEASE1'
+           // ibmiCommand "DLTF FILE($library/$savFile)"
+            //ibmiCommand "CRTSAVF QTEMP/BACKUP"
+            //ibmiCommand "SAVLIB LIB(ECTO1) DEV(*SAVF) SAVF(QTEMP/BACKUP)"
+            //ibmiGetSAVF library: "QTEMP", name: "BACKUP", toFile: "ecto1.savf"
+            
             //Create a library and carry on only if it exists
-            def result = ibmiCommand(command: "CRTLIB LIB($library)", failOnError: false)
+            def result = ibmiCommand(command: "CRTSAVF FILE($library/$savFile) TEXT('Backup before build ')", failOnError: false)
             if (!result.successful) {
                 if (result.getMessage('CPF2111') != null) {
-                    echo "Library $library already exists"
+                    echo " $savFile already exists"
     } else {
                     //Any other error is reported and stops the pipeline
                     error result.getPrettyMessages()
                 }
             }
 
-            //Delete a library; carry on whatever happens
-            ibmiCommand(command: "DLTLIB LIB($library)", failOnError: false)
-
-            def savfContent = ibmiGetSAVF(libray: 'QTEMP', name: 'BACKUP', toFile: 'backup.savf')
+           def result2 = ibmiCommand(command: "SAVLIB LIB(APINTO11) DEV(*SAVF) SAVF(APINTO12/RELEASE1) OMITOBJ(APINTO11/Q*)", failOnError:false)
+            def savfContent = ibmiGetSAVF(library: 'APINTO12', name: 'RELEASE1', toFile: 'release1.savf')
             //Check if the SAVF file exists
             if (savfContent == null) {
-                error "SAVF file not found"
+                error 'SAVF file not found'
             }
             //Check if the SAVF file is empty
             if (savfContent.entries.size() == 0) {
-                error "SAVF file is empty"
+                error 'SAVF file is empty'
             }
             //Print the number of objects in the SAVF file
             print "SAVF file contains ${savfContent.entries.size()} object(s)"
